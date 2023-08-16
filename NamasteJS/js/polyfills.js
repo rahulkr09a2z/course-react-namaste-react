32860ccc89e297d9ab87bd72115c67ee2586a9fc
rahulkr09a2z <rahulkr123a2z@gmail.com>
2023-08-16 15:28:30 +0530

    [NamasteJS]|1)Array Flatten polyfill implementation

--- NamasteJS/js/polyfills.js
+++ NamasteJS/js/polyfills.js
@@ -1,4 +1,5 @@
 var arr = [2, 1, 3, 5, 8, 4];
+var nestedArr = [1, [2, 3], [4, 5, [6, 7]], [8, 9]];
 
 Array.prototype.myMap = function (callback) {
   var arr = this;
@@ -38,6 +39,31 @@ Array.prototype.myReduce = function (callback, initialValue) {
   return acc;
 };
 
+Array.prototype.myFlatten = function (depth = "Infinity") {
+  var arr = this;
+  var newArr = [];
+
+  if (depth === "Infinity") depth = null;
+  function depthDecr(depth) {
+    if (depth === null) {
+      return null;
+    }
+    return depth - 1;
+  }
+
+  function flattenMethod(arr, depth) {
+    depth = depthDecr(depth);
+    for (let i = 0; i < arr.length; i++) {
+      let item = arr[i];
+      if (Array.isArray(item) && (depth > 0 || depth === null))
+        flattenMethod(item, depth);
+      else newArr.push(item);
+    }
+  }
+  flattenMethod(arr, depth);
+  return newArr;
+};
+
 console.log(
   "myMap double",
   arr.myMap((x) => x * 2)
@@ -51,10 +77,11 @@ console.log(
 console.log(
   "myReduce max",
   arr.myReduce((acc, curr) => {
-    console.log("zzz", acc, curr);
     if (curr > acc) {
       acc = curr;
     }
     return acc;
   }, 0)
 );
+
+console.log("myFlattern flatArr", nestedArr.myFlatten());
